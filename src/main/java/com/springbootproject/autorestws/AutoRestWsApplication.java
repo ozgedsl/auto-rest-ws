923ca8e2094f5ce33b8c09661e234a1cb7acb0d0
ozgenur.dasli <ozgenur.dasli@partner.yildizholding.com.tr>
2023-05-12 14:26:05 +0300

    fisrt commit

---
+++ src/main/java/com/springbootproject/autorestws/AutoRestWsApplication.java
@@ -0,0 +1,13 @@
+package com.springbootproject.autorestws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoRestWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoRestWsApplication.class, args);
+    }
+
+}
